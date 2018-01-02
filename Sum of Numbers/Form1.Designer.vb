0738fcae411a7f086927397bfec383288d72295b
Greg Pina <pinagm2825@gmail.com>
2018-01-02 05:27:09 -0500

    Initial commit

---
+++ Sum of Numbers/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNumbers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(25, 241)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(100, 25)
+        Me.btnEnter.TabIndex = 0
+        Me.btnEnter.Text = "Enter Numbers"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(356, 241)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 25)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(184, 241)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 25)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'frmNumbers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 310)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Name = "frmNumbers"
+        Me.Text = "Sum of Numbers"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ Sum of Numbers/Form1.vb
@@ -0,0 +1,81 @@
+﻿' **********************************************
+' Greg Pina
+' IT 101-401
+' Assignment 15 - Sum of Numbers
+' 11/11/17
+' **********************************************
+
+Option Strict On
+
+Public Class frmNumbers
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        'Here we get some variables to help with function
+
+        Dim intNumber As Integer
+        Dim intSum As Integer
+        Dim intCounter As Integer
+
+        'This creates the string to hold the numbers
+        Dim strNumber As String
+
+        Try
+
+            'This brings up the prompt
+            strNumber = InputBox("Enter a positive integer value.", "Input Needed", "10")
+
+
+
+            'Here we check the entered input
+            If strNumber <> String.Empty Then
+                intNumber = CInt(strNumber)
+            End If
+
+            If intNumber < 0 Then
+                MessageBox.Show("Negative numbers are not accepted.")
+                Exit Sub
+            End If
+
+            If IsNumeric(intNumber) = True Then
+                intNumber = CInt(strNumber)
+            Else
+                MessageBox.Show("Quantity amount must be numeric.")
+
+            End If
+
+            'Sum the numbers
+            For intCounter = 1 To intNumber
+                intSum += intCounter
+            Next
+
+            'Final Message Box
+            MessageBox.Show("The sum of numbers 1 through " & intNumber & " is " & intSum, "Sum of Numbers", MessageBoxButtons.OK)
+
+            Exit Sub
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        ' clears any data previously entered
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Closes the program
+        Close()
+
+    End Sub
+
+    Private Sub frmNumbers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
